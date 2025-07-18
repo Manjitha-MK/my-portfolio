eb1444de2d276d64229b262e08bfb9c97283c709
Manjitha-MK <manjithataridu123@gmail.com>
2025-07-18 12:23:47 +0530

    Changed <a> to <Link> for internal navigation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-scroll";
 import { Typewriter } from "react-simple-typewriter";
 
 export default function Hero() {
   return (
     <section className="flex flex-col justify-center items-center min-h-screen bg-gray-900 text-white px-4 text-center">
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">Hey, I'm Manjitha 👋</h1>
+      <h1 className="text-4xl md:text-6xl font-bold font-serif tracking-[4px] mb-4">Hey, I'm Manjitha 👋</h1>
       <h2 className="text-xl md:text-2xl text-indigo-400 mb-6">
         <Typewriter
           words={["Frontend Developer", "React Developer", "Tailwind Expert"]}
@@ -15,9 +16,9 @@ export default function Hero() {
           delaySpeed={1000}
         />
       </h2>
-      <a href="#projects" className="px-6 py-3 text-sm tracking-[2px] bg-indigo-600 hover:bg-indigo-700 rounded-lg mt-4">
+      <Link to="projects" className="px-6 py-3 text-sm tracking-[2px] cursor-pointer bg-indigo-600 hover:bg-indigo-700 rounded-lg mt-4">
         View Projects
-      </a>
+      </Link>
     </section>
   );
 }
