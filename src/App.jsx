262f27a0b34405dcd97b94cc70d6994498a7083c
Manjitha-MK <manjithataridu123@gmail.com>
2025-07-31 22:59:39 +0530

    add back button that navigates to home and smoothly scrolls to "projects" section using location state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import About from './components/About'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import Projects from './components/Project'
-import AdminHomePage from './Admin/adminhomepage'
-import ProjectOverview from './components/ProjectOverview'
-import AllProjects from './components/AllProjects'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Project";
+import AdminHomePage from "./Admin/adminhomepage";
+import ProjectOverview from "./components/ProjectOverview";
+import AllProjects from "./components/AllProjects";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function ScrollToSectionOnLoad() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const sectionId = location.state?.scrollTo;
+    if (sectionId) {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
@@ -22,22 +40,31 @@ function App() {
           path="/"
           element={
             <div className="bg-white text-gray-900 font-sans">
+              <ScrollToSectionOnLoad />
               <Navbar />
               <main className="pt-14">
-                <section id="home"><Hero /></section>
-                <section id="about"><About /></section>
-                <section id="projects"><Projects /></section>
-                <section id="contact"><Contact /></section>
+                <section id="home">
+                  <Hero />
+                </section>
+                <section id="about">
+                  <About />
+                </section>
+                <section id="projects">
+                  <Projects />
+                </section>
+                <section id="contact">
+                  <Contact />
+                </section>
               </main>
               <Footer />
             </div>
           }
         />
-        <Route path='/projectInfo/:id' element={<ProjectOverview/>} />
-        <Route path='/allprojects' element={<AllProjects/>} />
+        <Route path="/projectInfo/:id" element={<ProjectOverview />} />
+        <Route path="/allprojects" element={<AllProjects />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
